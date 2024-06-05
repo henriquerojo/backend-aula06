d910bf5a60639c787278210308d2c2da1aae54b8
henriquerojo <henriquerojo10@gmail.com>
2024-06-04 22:10:39 -0300

    criando requisições

--- routes/open_routes.js
+++ routes/open_routes.js
@@ -1,18 +1,55 @@
 import express from "express";
 const openRouter = express.Router();
+import bcrypt from "bcrypt";
+import User from "../database/user.js";
+import jwt from "jsonwebtoken";
 
 // [POST] - http://localhost:3000/auth/register
 openRouter.post("/register", async (request, response) => {
-  const body = request.body;
+  
+  try 
+  {
+    const { name, email, password } = request.body;
+    const userAleadyExists = await User.findOne({ email });
 
-  response.send(body);
+    if (userAleadyExists) 
+    {
+      return response.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    const saveUser = await newUser.save();
+
+    response.status(200).json(saveUser);
+  } 
+  catch (error) 
+  {
+    response.status(500).json({ error: error.message || "Error on create a new user" });
+  }
+
 });
 
 // [POST] - http://localhost:3000/auth/login
-openRouter.post("/login", (request, response) => {
-  const body = request.body;
+openRouter.post("/login", async(request, response) => {
+  const { email, password } = request.body;
 
-  response.send(body);
+  const user = await User.findOne({ email });
+  
+  if (!user)
+  {
+    return response.status(404).json({ message: "User not found" });
+  }
+
+  const passwordValid = await bcrypt.compare(password, user.password);
+  if (!passwordValid)
+  {
+    return response.status(400).json({ message: "Email or password incorrect" });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "24h" })
+
+  response.status(200).json({ token });
 });
 
 export { openRouter };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import "./database/connection.js";
 
 // Arquivos de rotas
 import { openRouter } from "./routes/open_routes.js";
@@ -9,11 +10,15 @@ import { protectedRouter } from "./routes/protected_routes.js";
 const app = express();
 const port = process.env.PORT;
 
-// Middlewares
+// Middlewares - Funções que são executadas antes da requisição chegar ao servidor (endpoint)
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", openRouter);
 app.use("/dashboard", protectedRouter);
+
+app.use((request, response) => {
+    response.status(404).json({message: "Page not found"});
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
