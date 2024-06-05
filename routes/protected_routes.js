0e392be4e03df4f7edc2c34eee303bf2f52223a4
henriquerojo <henriquerojo10@gmail.com>
2024-06-04 22:16:11 -0300

    implementando protected routes

--- routes/protected_routes.js
+++ routes/protected_routes.js
@@ -1,6 +1,14 @@
 import express from "express";
 const protectedRouter = express.Router();
 
+const validateToken = (request, response, next) => {
+  // pegar o token dos headers da request
+  // validar o token
+  // buscar o usuário no banco de dados com base no id que está no token
+  // qualquer erro retorna 400
+  next();
+};
+
 // [GET] - http://localhost:3000/dashboard
 protectedRouter.get("/", (request, response) => {
   response.send("Listagem dos dados protegidos");
